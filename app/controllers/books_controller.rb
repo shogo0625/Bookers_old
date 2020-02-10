5fe0cbd4b5511848da4ed2c7f17cca698c753657
shogo0625 <shogo19940625@gmail.com>
2020-02-10 08:21:58 +0000

    [Finish] サクセス/エラーメッセージの設定/完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,14 +3,19 @@ class BooksController < ApplicationController
   end
 
   def index
-  	@books = Book.all
+  	@books = Book.all.order(id: :asc)
   	@book = Book.new
   end
 
   def create
-  	book = Book.new(book_params)
-  	book.save
-  	redirect_to book_path(book)
+  	@book = Book.new(book_params)
+  	if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render 'index'
+    end
   end
 
   def show
@@ -22,14 +27,19 @@ class BooksController < ApplicationController
   end
 
   def update
-  	book = Book.find(params[:id])
-  	book.update(book_params)
-  	redirect_to book_path(book)
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
   	book = Book.find(params[:id])
   	book.destroy
+    flash[:notice] = "Book was successfully destroyed."
   	redirect_to books_path
   end
 
